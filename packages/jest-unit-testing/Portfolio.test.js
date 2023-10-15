82ccf6a8eb6671999e6068d9c6a6016c9ad66d14
lucawperri <lucawperri@gmail.com>
2023-10-15 16:18:38 -0700

    fixes

--- packages/jest-unit-testing/Portfolio.test.js
+++ packages/jest-unit-testing/Portfolio.test.js
@@ -8,7 +8,7 @@ test('isEmpty() returns true with 0 stocks', () => {
 
 test('isEmpty() returns false with > 1 stock', () => {
     const portfolio = new Portfolio();
-    portfolio.stocks.set("TSLA", 1);
+    portfolio.stocks.set("GME", 10);
     expect(portfolio.isEmpty()).toBeFalsy()
 });
 
@@ -19,75 +19,75 @@ test('numUniqueStocks() with 0 stocks', () => {
 
 test('numUniqueStocks() with 1 stock', () => {
     const portfolio = new Portfolio();
-    portfolio.stocks.set("TSLA", 1);
+    portfolio.stocks.set("GME", 10);
     expect(portfolio.numUniqueStocks()).toBe(1);
 });
 
 test('numUniqueStocks() with multiple stocks', () => {
     const portfolio = new Portfolio();
-    portfolio.stocks.set("TSLA", 1);
-    portfolio.stocks.set("RBLX", 5);
-    portfolio.stocks.set("RBLX", 6);
+    portfolio.stocks.set("GME", 10);
+    portfolio.stocks.set("SBUX", 7);
+    portfolio.stocks.set("SBUX", 5);
     expect(portfolio.numUniqueStocks()).toBe(2);
 });
 
 test('purchase() a new stock', () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    expect(portfolio.checkHolding("TSLA")).toBe(1);
+    portfolio.purchase("GME", 8);
+    expect(portfolio.checkHolding("GME")).toBe(8);
 });
 
 test('purchase() one stock multiple times', () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    portfolio.purchase("TSLA", 3);
-    expect(portfolio.checkHolding("TSLA")).toBe(4);
+    portfolio.purchase("GME", 8);
+    portfolio.purchase("GME", 3);
+    expect(portfolio.checkHolding("GME")).toBe(11);
 });
 
 test('purchase() multiple stocks multiple times', () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    portfolio.purchase("RBLX", 10);
-    portfolio.purchase("TSLA", 3);
-    portfolio.purchase("RBLX", 0.5);
+    portfolio.purchase("GME", 2);
+    portfolio.purchase("SBUX", 8);
+    portfolio.purchase("GME", 3);
+    portfolio.purchase("SBUX", 0.5);
 
-    expect(portfolio.checkHolding("TSLA")).toBe(4);
-    expect(portfolio.checkHolding("RBLX")).toBe(10.5);
-});
+    expect(portfolio.checkHolding("GME")).toBe(5);
+    expect(portfolio.checkHolding("SBUX")).toBe(8.5);
+});8
 
 test('sell() a stock that exists', () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 2);
-    portfolio.sell("TSLA", 1);
-    expect(portfolio.checkHolding("TSLA")).toBe(1);
+    portfolio.purchase("GME", 2);
+    portfolio.sell("GME", 1);
+    expect(portfolio.checkHolding("GME")).toBe(1);
 });
 
 test("sell() a stock that doesn't exist", () => {
     const portfolio = new Portfolio();
-    expect(() => portfolio.sell("TSLA", 1)).toThrow("Stock does not exist");
+    expect(() => portfolio.sell("GME", 1)).toThrow("Stock does not exist");
 });
 
 test("sell() more of a stock than is available", () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    expect(() => portfolio.sell("TSLA", 2)).toThrow("ShareSaleException");
+    portfolio.purchase("GME", 1);
+    expect(() => portfolio.sell("GME", 2)).toThrow("ShareSaleException");
 });
 
 test("sell() all of a stock deletes it from the portfolio", () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    portfolio.sell("TSLA", 1);
+    portfolio.purchase("GME", 1);
+    portfolio.sell("GME", 1);
     expect(portfolio.isEmpty()).toBeTruthy();
 });
 
 test("checkHolding() for a stock that exists", () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    expect(portfolio.checkHolding("TSLA")).toBe(1);
+    portfolio.purchase("GME", 1);
+    expect(portfolio.checkHolding("GME")).toBe(1);
 });
 
 test("checkHolding() for a stock that does not exist", () => {
     const portfolio = new Portfolio();
-    portfolio.purchase("TSLA", 1);
-    expect(() => portfolio.checkHolding("RBLX")).toThrow("Stock does not exist");
+    portfolio.purchase("GME", 1);
+    expect(() => portfolio.checkHolding("SBUX")).toThrow("Stock does not exist");
 });
